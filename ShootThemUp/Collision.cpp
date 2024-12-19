4bc11634f34d1cb3e7cb40fb1d429b99727d51f9
Drast <157280628+Drastundra@users.noreply.github.com>
2024-12-19 08:16:37 +0100

    Delete ShootThemUp directory

--- ShootThemUp/Collision.cpp
+++
@@ -1,5 +0,0 @@
-#include "Collision.h"
-
-bool Collision::AABB(const sf::Sprite& obj1, const sf::Sprite& obj2) {
-    return obj1.getGlobalBounds().intersects(obj2.getGlobalBounds());
-}
--- ShootThemUp/Collision.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <SFML/Graphics.hpp>
-
-class Collision {
-public:
-    static bool AABB(const sf::Sprite& obj1, const sf::Sprite& obj2);
-};
